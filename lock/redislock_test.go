fee995d9eb9b07e1168dd29ea27aa9da3ee99217
15ho <fifteenho@163.com>
2025-03-29 10:20:03 +0800

    feat: add lock test

--- lock/redislock_test.go
+++ lock/redislock_test.go
@@ -1,3 +1,56 @@
 package lock
 
-// TODO: test
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRedisLocker(t *testing.T) {
+	s := assert.New(t)
+
+	client := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	if !s.NoError(client.FlushAll(context.Background()).Err()) {
+		return
+	}
+	defer client.Close()
+
+	locker, err := NewRedisLocker(client,
+		WithRedisLockerAcquireInterval(200*time.Millisecond),
+		WithRedisLockerMaxAcquireCount(5),
+		WithRedisLockerKeyPrefix("test:"),
+		WithRedisLockerLockTTL(10*time.Second))
+
+	if !s.NoError(err) {
+		return
+	}
+
+	ch := make(chan Unlocker)
+
+	go func() {
+		unlocker, err := locker.Lock(context.Background(), "key1")
+		if !s.NoError(err) {
+			close(ch)
+			return
+		}
+		ch <- unlocker
+		_, err = locker.Lock(context.Background(), "key2")
+		s.NoError(err)
+	}()
+
+	unlocker, ok := <-ch
+	if !s.True(ok) {
+		return
+	}
+
+	if !s.NoError(unlocker.Unlock(context.Background())) {
+		return
+	}
+
+	s.NoError(locker.Unlock(context.Background(), "key2"))
+}
